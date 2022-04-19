29f8483c2dab9bc2aae04fc368f6405a2767dc33
root <root@40186.example.ru>
2022-04-19 17:28:35 +0300

    datasets removed legacy

--- datasets.py
+++ datasets.py
@@ -1,9 +1,11 @@
-from torch.utils.data import Dataset
-import cv2
-from torchvision import transforms
-import torch
-import numpy as np
 import os
+
+import cv2
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+
 
 transform = transforms.Compose([
     transforms.ToPILImage(),
@@ -18,6 +20,7 @@ transform1 = transforms.Compose([
     transforms.ToTensor()
 ])
 
+
 class CelebaBinaryCalssification(Dataset):
     def __init__(self, images, attributes_list, annots, class_name, transform=False):
         self.images = sorted(images)
@@ -27,7 +30,7 @@ class CelebaBinaryCalssification(Dataset):
         self.transfom = transform
     def __len__(self):
         return len(self.images)
-    def __getitem__(self, idx):        
+    def __getitem__(self, idx):
         im_name = self.images[idx]
         target = self.annots[im_name.split('/')[-1]][self.attributes_list.index(self.class_name)]
         image = np.zeros((256, 256, 3), dtype = np.uint8)
@@ -40,19 +43,20 @@ class CelebaBinaryCalssification(Dataset):
             image = transform(image).float()
         return image, int((target + 1) // 2)
 
+
 class CelebaSegmentation(Dataset):
     def __init__(self, images, class_name):
         self.images = sorted(images)
-        self.class_name = class_name   
+        self.class_name = class_name
     def __len__(self):
-        return len(self.images)  
-    def __getitem__(self, idx):        
+        return len(self.images)
+    def __getitem__(self, idx):
         im_name = self.images[idx]
         num = im_name.split('/')[-1].split('.')[0]
         num = int(num)
         # check
         segm_name = "/root/dmartynov/CelebAMask-HQ/CelebAMask-HQ-mask-anno/" + str(num//2000) + '/' + str(num).zfill(5) + "_" + self.class_name + ".png"
-        image = cv2.imread(im_name)  
+        image = cv2.imread(im_name)
         image = transform(image).float()
         if os.path.isfile(segm_name):
             segm_image = cv2.imread(segm_name, cv2.IMREAD_GRAYSCALE)
@@ -63,137 +67,5 @@ class CelebaSegmentation(Dataset):
             # print("no mask")
         segm_image = transform(segm_image).float()
         segm_image = (segm_image > 0).float()
+
         return image, segm_image
-
-# LEGACY
-class CelebaMale(Dataset):
-    def __init__(self, images, attributes_list, annots):
-        self.images = sorted(images)
-        self.attributes_list = attributes_list
-        self.annots = annots
-    def __len__(self):
-        return len(self.images)
-    def __getitem__(self, idx):        
-        im_name = self.images[idx]
-        target = self.annots[im_name.split('/')[-1]][self.attributes_list.index('Male')]
-        image = np.zeros((256, 256, 3), dtype = np.uint8)
-        shift_x = (256 - 218) // 2
-        shift_y = (256 - 178) // 2
-        image[shift_x: -shift_x, shift_y: -shift_y] = cv2.imread(im_name)       
-        image = transform(image).float()
-        return image, int((target + 1) // 2)
-
-class CelebaSmile(Dataset):
-    def __init__(self, images, attributes_list, annots):
-        self.images = sorted(images)
-        self.attributes_list = attributes_list
-        self.annots = annots
-    def __len__(self):
-        return len(self.images)
-    def __getitem__(self, idx):        
-        im_name = self.images[idx]
-        target = self.annots[im_name.split('/')[-1]][self.attributes_list.index('Smiling')]
-        image = np.zeros((256, 256, 3), dtype = np.uint8)
-        shift_x = (256 - 218) // 2
-        shift_y = (256 - 178) // 2
-        image[shift_x: -shift_x, shift_y: -shift_y] = cv2.imread(im_name)     
-        image = transform(image).float()
-        return image, int((target + 1) // 2)
-
-class CelebaYoung(Dataset): # derm3.py
-    def __init__(self, images, attributes_list, annots):
-        self.images = sorted(images)
-        self.attributes_list = attributes_list
-        self.annots = annots
-    def __len__(self):
-        return len(self.images)   
-    def __getitem__(self, idx):        
-        im_name = self.images[idx]
-        target = self.annots[im_name.split('/')[-1]][self.attributes_list.index('Young')]      
-        image = np.zeros((256, 256, 3), dtype = np.uint8)
-        shift_x = (256 - 218) // 2
-        shift_y = (256 - 178) // 2
-        image[shift_x: -shift_x, shift_y: -shift_y] = cv2.imread(im_name)    
-        image = transform(image).float()
-        return image, int((target + 1) // 2)
-
-class CelebaIdentity(Dataset): # derm5.ipynb
-    def __init__(self, images, identity):
-        self.images = sorted(images)    
-        self.identity = identity
-    def __len__(self):
-        return len(self.images)   
-    def __getitem__(self, idx):        
-        im_name = self.images[idx]
-        ident =  self.identity[im_name.split('/')[-1]]        
-        image = np.zeros((256, 256, 3), dtype = np.uint8)
-        shift_x = (256 - 218) // 2
-        shift_y = (256 - 178) // 2
-        image[shift_x: -shift_x, shift_y: -shift_y] = cv2.imread(im_name)       
-        image = transform(image).float()
-        return image, ident
-
-class CelebaLandmarks(Dataset): # derm9.ipynb
-    def __init__(self, images, landmarks):
-        self.images = sorted(images)    
-        self.landmarks = landmarks
-    def __len__(self):
-        return len(self.images)   
-    def __getitem__(self, idx):        
-        im_name = self.images[idx]
-        im_landmarks = self.landmarks[im_name.split('/')[-1]]        
-        image = np.zeros((256, 256, 3), dtype = np.uint8)
-        shift_x = (256 - 218) // 2
-        shift_y = (256 - 178) // 2
-        image[shift_x: -shift_x, shift_y: -shift_y] = cv2.imread(im_name)        
-        image = transform(image).float()
-        return image, np.float32(im_landmarks / 256.)
-
-class CelebaNose(Dataset):
-    def __init__(self, images):
-        self.images = sorted(images)   
-    def __len__(self):
-        return len(self.images)  
-    def __getitem__(self, idx):        
-        im_name = self.images[idx]
-        # print(im_name, idx)
-        num = im_name.split('/')[-1].split('.')[0]
-        num = int(num)
-        segm_name = "/root/dmartynov/CelebAMask-HQ/CelebAMask-HQ-mask-anno/" + str(num//2000) + '/' + str(num).zfill(5) + "_nose.png"
-        image = cv2.imread(im_name)  
-        image = transform(image).float()
-        try:
-            segm_image = cv2.imread(segm_name, cv2.IMREAD_GRAYSCALE)
-        except:
-            segm_image = None
-        if segm_image is None: # file not found == no nose
-            segm_image = torch.zeros((1, 256, 256))
-            print("no nose")
-        segm_image = transform(segm_image).float()
-        segm_image = (segm_image > 0).float()
-        return image, segm_image
-
-class CelebaSkin(Dataset):
-    def __init__(self, images):
-        self.images = sorted(images)   
-    def __len__(self):
-        return len(self.images)  
-    def __getitem__(self, idx):        
-        im_name = self.images[idx]
-        # print(im_name, idx)
-        num = im_name.split('/')[-1].split('.')[0]
-        num = int(num)
-        segm_name = "/root/dmartynov/CelebAMask-HQ/CelebAMask-HQ-mask-anno/" + str(num//2000) + '/' + str(num).zfill(5) + "_skin.png"
-        image = cv2.imread(im_name)  
-        image = transform(image).float()
-        try:
-            segm_image = cv2.imread(segm_name, cv2.IMREAD_GRAYSCALE)
-        except:
-            segm_image = None
-        if segm_image is None: # file not found == no nose
-            segm_image = torch.zeros((1, 256, 256))
-            print("no skin")
-        segm_image = transform(segm_image).float()
-        segm_image = (segm_image > 0).float()
-        return image, segm_image
-
